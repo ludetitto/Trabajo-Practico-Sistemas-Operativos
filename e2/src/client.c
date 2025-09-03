de497c047da962db19e7018351a673b9e772e940
franvigna <69130363+franvigna@users.noreply.github.com>
2025-09-02 23:21:50 -0300

    modificaciones en e2 principio para modificar y orientar el codigo a cola de espera

--- e2/src/client.c
+++ e2/src/client.c
@@ -1,76 +1,57 @@
-#include "../include/common.h"
-
-static void usage(const char *p)
-{
-  fprintf(stderr, "Uso: %s -h <host> -p <puerto>\n", p);
-}
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char **argv)
 {
   const char *host = "127.0.0.1";
-  int port = 0;
+  int port = 5000;
   for (int i = 1; i < argc; i++)
   {
     if (!strcmp(argv[i], "-h") && i + 1 < argc)
       host = argv[++i];
     else if (!strcmp(argv[i], "-p") && i + 1 < argc)
       port = atoi(argv[++i]);
-    else if (!strcmp(argv[i], "--help"))
-    {
-      usage(argv[0]);
-      return 0;
-    }
   }
-  if (port <= 0)
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in sa = {0};
+  sa.sin_family = AF_INET;
+  sa.sin_port = htons(port);
+  sa.sin_addr.s_addr = inet_addr(host);
+  if (connect(fd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
   {
-    usage(argv[0]);
+    perror("connect");
     return 1;
   }
-
-  int s = socket(AF_INET, SOCK_STREAM, 0); // socket TCP
-  if (s < 0)
-    die("socket");
-  struct sockaddr_in a = {0};
-  a.sin_family = AF_INET;
-  a.sin_port = htons(port);
-  if (inet_pton(AF_INET, host, &a.sin_addr) <= 0)
-    die("host");
-  if (connect(s, (struct sockaddr *)&a, sizeof(a)) < 0) // conectar
-    die("connect");
-  FILE *io = fdopen(s, "r+"); // stream para leer/escribir
-  if (!io)
-    die("fdopen");
-  setvbuf(io, NULL, _IOLBF, 0); // buffer de línea
-
-  printf("Conectado a %s:%d. Comandos: PING | GET <id> | FIND nombre=VAL | BEGIN | ADD nombre=.. generador=.. pid=.. | UPDATE id=.. [..] | DELETE id=.. | COMMIT | ROLLBACK | QUIT\n", host, port);
-
-  char in[LINE_MAX], out[LINE_MAX];
-  while (1)
+  char buf[1024];
+  int n;
+  n = (int)read(fd, buf, sizeof(buf) - 1);
+  if (n > 0)
   {
-    printf("> ");
-    fflush(stdout);
-    if (!fgets(in, sizeof(in), stdin))
-      break;
-    trim_nl(in);
-    if (!*in)
-      continue;
-    fprintf(io, "%s\n", in);
-    fflush(io);
-    // leer respuesta (puede ser multi-línea)
-    while (fgets(out, sizeof(out), io))
-    {
-      fputs(out, stdout);
-      if (!strncmp(out, "OK", 2) || !strncmp(out, "ERR", 3) || !strncmp(out, "BYE", 3))
-        break;
-      if (!strncmp(out, "END", 3))
-        break;
-      if (!strncmp(out, "RESULT", 6))
-        continue; // seguir leyendo
-    }
-    if (!strcasecmp(in, "QUIT"))
-      break;
+    buf[n] = 0;
+    fputs(buf, stdout);
   }
 
-  fclose(io);
+  while (fgets(buf, sizeof(buf), stdin))
+  {
+    ssize_t sent = write(fd, buf, strlen(buf));
+    if (sent < 0)
+    {
+      perror("write");
+      break;
+    } // maneja error de envío
+
+    int n = (int)read(fd, buf, sizeof(buf) - 1);
+    if (n <= 0)
+      break;
+    buf[n] = 0;
+    fputs(buf, stdout);
+    ;
+  }
+  close(fd);
   return 0;
 }
